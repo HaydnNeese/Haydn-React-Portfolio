6246c6d0e55d10b8aa208e5564efc6024639a231
HaydnNeese <haydnneese@yahoo.com>
2019-06-10 09:35:19 -0500

    current updates

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Paragraph = () => {
+    return(
+        <div>
+            <p>
+                I started my journey a little different than your typical web developer. I was working as a personal trainer, getting to use my college degree
+                every day and helping with their health and wellness goals. I met two clients that were web developers and trained them for a year each and 
+                discovered through conversation multiple times a week that I was in the wrong career, no 
+            </p>
+        </div>
+    )
+}
